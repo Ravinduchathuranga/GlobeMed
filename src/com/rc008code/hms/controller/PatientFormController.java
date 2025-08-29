5e7529d1d93fd552ccabb225268fafdf2e53295b
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-08-29 22:42:23 +0530

    Update PatientFormController.java

--- src/com/rc008code/hms/controller/PatientFormController.java
+++ src/com/rc008code/hms/controller/PatientFormController.java
@@ -1,9 +1,16 @@
 package com.rc008code.hms.controller;
 
+import com.rc008code.hms.database.DbConnection;
+import com.rc008code.hms.entity.Patient;
+import com.rc008code.hms.enums.Gender;
+import com.rc008code.hms.util.AlertBuilder;
 import com.rc008code.hms.util.CommonUtil;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class PatientFormController {
 
@@ -32,13 +39,49 @@ public class PatientFormController {
     public TableColumn columnOptions;
 
     // private properties
-    private CommonUtil commonUtil=new CommonUtil();
+    private CommonUtil commonUtil = new CommonUtil();
+
     public void initialize() {
+        loadAllPatients();
     }
 
+    private void loadAllPatients(){
+        
+    }
+    // patient creation
     public void onCreateAction(ActionEvent actionEvent) {
+        try {
+
+            Patient patient = new Patient(
+                    UUID.randomUUID().toString(),
+                    txtName.getText(),
+                    txtAddress.getText(),
+                    Integer.parseInt(txtAge.getText()),
+                    rbtnMale.isSelected() ? Gender.MALE : Gender.FEMALE,
+                    txtContactNo.getText(),
+                    txtEmail.getText()
+            );
+            DbConnection dbConnection = new DbConnection();
+            boolean isSaved = dbConnection.createPatient(patient);
+            if (isSaved) {
+                new Alert(Alert.AlertType.INFORMATION, "Patient Created Successfully", ButtonType.OK).show();
+                clearFields();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void onBackToHomeAction(ActionEvent actionEvent) {
+    public void onBackToHomeAction(ActionEvent actionEvent) throws IOException {
+        commonUtil.setUi(context,"Dashboard");
+    }
+
+    private void clearFields() {
+        txtName.clear();
+        txtAddress.clear();
+        txtAge.clear();
+        txtContactNo.clear();
+        txtEmail.clear();
     }
 }
