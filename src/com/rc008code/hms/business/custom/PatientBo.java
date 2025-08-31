3eec6f74cdccd196a4a55b6d3e7e9735b9fb3cfc
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-08-31 09:46:41 +0530

    Update PatientBo.java

--- src/com/rc008code/hms/business/custom/PatientBo.java
+++ src/com/rc008code/hms/business/custom/PatientBo.java
@@ -1,4 +1,15 @@
 package com.rc008code.hms.business.custom;
 
+
+import com.rc008code.hms.dto.PatientDto;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface PatientBo {
+    public boolean create(PatientDto entity) throws SQLException, ClassNotFoundException;
+    public PatientDto read(String id) throws SQLException, ClassNotFoundException;
+    public boolean update(PatientDto entity) throws SQLException, ClassNotFoundException;
+    public boolean delete(String id) throws SQLException, ClassNotFoundException;
+    public List<PatientDto> readAll() throws SQLException, ClassNotFoundException;
 }
