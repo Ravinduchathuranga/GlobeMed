613b1e78ae0a4b6669d7d6845e97760c0411c238
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-08-31 09:47:08 +0530

    Create DaoFactory.java

---
+++ src/com/rc008code/hms/repository/DaoFactory.java
@@ -0,0 +1,42 @@
+package com.rc008code.hms.repository;
+
+import com.rc008code.hms.repository.custom.impl.*;
+
+public class DaoFactory {
+
+    private static DaoFactory daoFactory;
+
+    private DaoFactory() {
+    }
+
+    public static DaoFactory getInstance() {
+        if (daoFactory == null) {
+            daoFactory = new DaoFactory();
+        }
+        return daoFactory;
+    }
+
+    public enum DaoType {
+        DOCTOR, NURSE, PATIENT, PHARMACIST, APPOINTMENT, MEDICINE
+    }
+    
+    @SuppressWarnings("unchecked")
+    public <T> T getDao(DaoType daoType) {
+        switch (daoType) {
+            case DOCTOR:
+                return (T) new DoctorDaoImpl();
+            case NURSE:
+                return (T) new NurseDaoImpl();
+            case PATIENT:
+                return (T) new PatientDaoImpl();
+            case PHARMACIST:
+                return (T) new PharmacistDaoImpl();
+//            case APPOINTMENT:
+//                return (T) new AppointmentDaoImpl();
+//            case MEDICINE:
+//                return (T) new MedicineDaoImpl();
+            default:
+                throw new IllegalArgumentException("Unknown DAO type: " + daoType);
+        }
+    }
+}
