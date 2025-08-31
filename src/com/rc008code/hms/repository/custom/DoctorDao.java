d8d2f82c42ec0e6a21c70afc3fa2367f27a7f1d2
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-08-31 08:04:39 +0530

    Create DoctorDao.java

---
+++ src/com/rc008code/hms/repository/custom/DoctorDao.java
@@ -0,0 +1,7 @@
+package com.rc008code.hms.repository.custom;
+
+import com.rc008code.hms.entity.Doctor;
+import com.rc008code.hms.repository.CrudDao;
+
+public interface DoctorDao extends CrudDao<Doctor, String> {
+}
