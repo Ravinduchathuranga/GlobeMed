17bf1db89dc933a985ac09d7e64fb6a6ae374b93
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-08-31 08:04:29 +0530

    Update PatientDaoImpl.java

--- src/com/rc008code/hms/repository/custom/impl/PatientDaoImpl.java
+++ src/com/rc008code/hms/repository/custom/impl/PatientDaoImpl.java
@@ -1,6 +1,35 @@
 package com.rc008code.hms.repository.custom.impl;
 
+import com.rc008code.hms.entity.Patient;
 import com.rc008code.hms.repository.custom.PatientDao;
 
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+
 public class PatientDaoImpl implements PatientDao {
+    @Override
+    public boolean create(Patient patient) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Patient find(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean update(Patient patient) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public List<Patient> findAll() throws SQLException, ClassNotFoundException {
+        return Collections.emptyList();
+    }
 }
