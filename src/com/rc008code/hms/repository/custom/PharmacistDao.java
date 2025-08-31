e3b69362b0cd54fe2972f9d2eb32d50fe796142b
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-08-31 08:04:16 +0530

    Create PharmacistDao.java

---
+++ src/com/rc008code/hms/repository/custom/PharmacistDao.java
@@ -0,0 +1,7 @@
+package com.rc008code.hms.repository.custom;
+
+import com.rc008code.hms.entity.Pharmacist;
+import com.rc008code.hms.repository.CrudDao;
+
+public interface PharmacistDao extends CrudDao<Pharmacist, String> {
+}
