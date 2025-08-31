cdf5b8d6c33a6c49ff3a2f3ef898b965260c833e
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-08-31 09:47:03 +0530

    Update NurseDao.java

--- src/com/rc008code/hms/repository/custom/NurseDao.java
+++ src/com/rc008code/hms/repository/custom/NurseDao.java
@@ -3,5 +3,8 @@ package com.rc008code.hms.repository.custom;
 import com.rc008code.hms.entity.Nurse;
 import com.rc008code.hms.repository.CrudDao;
 
+import java.util.List;
+
 public interface NurseDao extends CrudDao<Nurse, String> {
+    List<Nurse> searchNurse(String searchText) throws Exception;
 }
