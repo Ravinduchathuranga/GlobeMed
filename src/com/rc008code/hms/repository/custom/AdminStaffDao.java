e27bbf0fc7582fcb80ec1b09b33d223d8544ec81
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-19 09:06:55 +0530

    Update AdminStaffDao.java

--- src/com/rc008code/hms/repository/custom/AdminStaffDao.java
+++ src/com/rc008code/hms/repository/custom/AdminStaffDao.java
@@ -6,5 +6,6 @@ import com.rc008code.hms.repository.CrudDao;
 import java.util.List;
 
 public interface AdminStaffDao extends CrudDao<AdminStaff, String> {
+    boolean logIn(String username, String password) throws Exception;
     List<AdminStaff> searchAdminStaff(String searchText) throws Exception;
 }
