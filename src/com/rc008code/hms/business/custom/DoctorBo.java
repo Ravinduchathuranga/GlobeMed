5bb6c27a9612c9437f859f08879902968faeedc2
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-19 09:06:47 +0530

    Update DoctorBo.java

--- src/com/rc008code/hms/business/custom/DoctorBo.java
+++ src/com/rc008code/hms/business/custom/DoctorBo.java
@@ -7,10 +7,15 @@ import java.util.List;
 
 public interface DoctorBo {
     public boolean create(DoctorDto entity) throws SQLException, ClassNotFoundException;
+
     public DoctorDto read(String id) throws SQLException, ClassNotFoundException;
+
     public boolean update(DoctorDto entity) throws SQLException, ClassNotFoundException;
+
     public boolean delete(String id) throws SQLException, ClassNotFoundException;
+
     public List<DoctorDto> readAll() throws SQLException, ClassNotFoundException;
+
     public List<DoctorDto> search(String searchText) throws SQLException, ClassNotFoundException;
 
 }
