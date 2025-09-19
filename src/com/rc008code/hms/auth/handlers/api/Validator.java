bbdff79ef9c390d843c4775623e7109cb280b3a1
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-19 10:25:18 +0530

    Update Validator.java

--- src/com/rc008code/hms/auth/handlers/api/Validator.java
+++ src/com/rc008code/hms/auth/handlers/api/Validator.java
@@ -2,6 +2,7 @@ package com.rc008code.hms.auth.handlers.api;
 
 import com.rc008code.hms.auth.handlers.model.ValidationResult;
 
+//input fields validation
 public interface Validator<T> {
     ValidationResult validate(T t);
     void setNext(Validator<T> next);
