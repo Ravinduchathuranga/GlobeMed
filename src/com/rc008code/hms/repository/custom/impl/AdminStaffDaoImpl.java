f2bb7447bba03d9012fb561db60894d66d0644dd
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-19 09:06:59 +0530

    Update AdminStaffDaoImpl.java

--- src/com/rc008code/hms/repository/custom/impl/AdminStaffDaoImpl.java
+++ src/com/rc008code/hms/repository/custom/impl/AdminStaffDaoImpl.java
@@ -1,9 +1,13 @@
 package com.rc008code.hms.repository.custom.impl;
 
+import com.rc008code.hms.database.MysqlConnection;
 import com.rc008code.hms.entity.AdminStaff;
+import com.rc008code.hms.repository.CrudUtil;
 import com.rc008code.hms.repository.custom.AdminStaffDao;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,5 +40,18 @@ public class AdminStaffDaoImpl implements AdminStaffDao {
     @Override
     public List<AdminStaff> searchAdminStaff(String searchText) throws Exception {
         return Collections.emptyList();
+    }
+
+    @Override
+    public boolean logIn(String username, String password) throws Exception {
+        String query = "SELECT * FROM admin_staff WHERE username = '" + username + "'AND password = '" + password + "'";
+        System.out.println(query);
+        Statement statement = MysqlConnection.getInstance().getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        if (resultSet.next()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
