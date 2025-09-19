94aa8a1e9dc42b279b350d1cdee16ace17442b4f
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-19 08:22:36 +0530

    Update AppointmentBo.java

--- src/com/rc008code/hms/business/custom/AppointmentBo.java
+++ src/com/rc008code/hms/business/custom/AppointmentBo.java
@@ -6,12 +6,17 @@ import com.rc008code.hms.dto.DoctorDto;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface AppointmentBo  {
+public interface AppointmentBo {
     public boolean create(AppointmentDto entity) throws SQLException, ClassNotFoundException;
+
     public DoctorDto read(String id) throws SQLException, ClassNotFoundException;
+
     public boolean update(AppointmentDto entity) throws SQLException, ClassNotFoundException;
+
     public boolean delete(String id) throws SQLException, ClassNotFoundException;
+
     public List<AppointmentDto> readAll() throws SQLException, ClassNotFoundException;
+
     public List<AppointmentDto> search(String searchText) throws SQLException, ClassNotFoundException;
 
 }
