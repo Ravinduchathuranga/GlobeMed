73f5434fd018f67971fce4631317c8a1bdb0a860
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-19 08:22:30 +0530

    Create LoggingAuthenticator.java

---
+++ src/com/rc008code/hms/auth/decorators/service/LoggingAuthenticator.java
@@ -0,0 +1,17 @@
+package com.rc008code.hms.auth.decorators.service;
+
+import com.rc008code.hms.auth.decorators.api.Authenticator;
+import com.rc008code.hms.auth.decorators.api.AuthenticatorDecorator;
+
+public class LoggingAuthenticator extends AuthenticatorDecorator {
+    public LoggingAuthenticator(Authenticator authenticator) {
+        super(authenticator);
+    }
+
+    @Override
+    public boolean authenticate(String username, String password) {
+        boolean result = super.authenticate(username, password);
+        System.out.println("Logging Authenticator: " + username + " " + password + " " + result);
+        return result;
+    }
+}
