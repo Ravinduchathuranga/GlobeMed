7e2f699209d487daa769fb583de6e1bd61e81291
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-22 14:35:55 +0530

    Update Test.java

--- src/com/rc008code/hms/Test.java
+++ src/com/rc008code/hms/Test.java
@@ -1,16 +1,55 @@
 package com.rc008code.hms;
+
 import com.rc008code.hms.business.BoFactory;
-import com.rc008code.hms.business.custom.AdminStaffBo;
+import com.rc008code.hms.business.custom.MedicalRecordBo;
+import com.rc008code.hms.dto.MedicalRecordDto;
+import com.rc008code.hms.enums.Departments;
+
+import java.util.Date;
+import java.util.List;
+
 
 public class Test {
-    private final static AdminStaffBo adminStaffBo= BoFactory.getInstance().getBo(BoFactory.BoType.ADMINSTAFF);
-    public static void main(String[] args) {
-        try {
-            boolean isLogin= adminStaffBo.logIn("ravindu@gmail.com","12345");
-            System.out.println(isLogin);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+    private static MedicalRecordBo medicalRecordBo = BoFactory.getInstance().getBo(BoFactory.BoType.MEDICALRECORD);
 
+    public static void main(String[] args) throws Exception {
+        testMedicalRecords();
     }
+
+    private static void testMedicalRecords() throws Exception {
+        System.out.println("\n=== Testing Doctor Operations ===");
+
+        // Create Doctor
+        MedicalRecordDto medicalRecordDto = new MedicalRecordDto(
+                "212121",
+                "P554f2",
+                "e4a11581-defe-4ac3-8b26-bb374f665833",
+                "0987654321",
+                "smith@test.com",
+                new Date()
+        );
+
+        boolean created = medicalRecordBo.create(medicalRecordDto);
+        System.out.println("Create Doctor: " + (created ? "SUCCESS" : "FAILED"));
+
+//        // Get Doctor
+//        MedicalRecordDto retrievedDoctor = medicalRecordBo.read("212121");
+//        System.out.println("Get Doctor: " + (retrievedDoctor != null ? "SUCCESS" : "FAILED"));
+//
+//        // Update Doctor
+//        if (retrievedDoctor != null) {
+//            retrievedDoctor.setDiagnosis("Dr. Smith Updated");
+//            boolean updated = medicalRecordBo.update(retrievedDoctor);
+//            System.out.println("Update Doctor: " + (updated ? "SUCCESS" : "FAILED"));
+//        }
+//
+//        // List all Doctors
+//        List<MedicalRecordDto> doctors = medicalRecordBo.readAll();
+//        System.out.println("List Doctors: Found " + doctors.size() + " doctors");
+//
+//        // Cleanup
+//        boolean deleted = medicalRecordBo.delete("212121");
+//        System.out.println("Delete Doctor: " + (deleted ? "SUCCESS" : "FAILED"));
+    }
+
 }
