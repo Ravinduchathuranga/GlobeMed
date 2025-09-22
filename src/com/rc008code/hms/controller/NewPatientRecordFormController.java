25e7f47553d8e4e34a4ae04e4b2adf62176d2f01
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-22 19:47:42 +0530

    Update NewPatientRecordFormController.java

--- src/com/rc008code/hms/controller/NewPatientRecordFormController.java
+++ src/com/rc008code/hms/controller/NewPatientRecordFormController.java
@@ -1,4 +1,16 @@
 package com.rc008code.hms.controller;
 
+import com.rc008code.hms.util.CommonUtil;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class NewPatientRecordFormController {
+
+    public AnchorPane context;
+
+    public void onBackAction(ActionEvent event) throws IOException {
+        new CommonUtil().setUi(context,"DoctorDashboardForm");
+    }
 }
