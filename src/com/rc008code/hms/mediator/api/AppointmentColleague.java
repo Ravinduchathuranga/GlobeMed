b8a08917a6bbb9951bec14ff9e9be4f93bbc61a0
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-23 22:27:29 +0530

    Create AppointmentColleague.java

---
+++ src/com/rc008code/hms/mediator/api/AppointmentColleague.java
@@ -0,0 +1,8 @@
+package com.rc008code.hms.mediator.api;
+
+/**
+ * Base colleague interface for components that participate in appointment scheduling.
+ */
+public interface AppointmentColleague {
+    void setMediator(AppointmentMediator mediator);
+}
