a295dc67f7ef138da2e955b9139dc4e8c0495a75
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-23 19:27:59 +0530

    Update AuthenticatorDecorator.java

--- src/com/rc008code/hms/auth/decorators/service/decorators/AuthenticatorDecorator.java
+++ src/com/rc008code/hms/auth/decorators/service/decorators/AuthenticatorDecorator.java
@@ -3,17 +3,9 @@ package com.rc008code.hms.auth.decorators.service.decorators;
 import com.rc008code.hms.auth.decorators.api.Authenticator;
 
 public abstract class AuthenticatorDecorator implements Authenticator {
-
-    //dto logic here
-
     protected Authenticator authenticator;
 
     public AuthenticatorDecorator(Authenticator authenticator) {
         this.authenticator = authenticator;
-    }
-
-    @Override
-    public boolean authenticate(String username, String password) {
-        return authenticator.authenticate(username, password);
     }
 }
