03740221d4a6eb4ddd486f91ebe9a4849e3fbb64
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-23 19:28:08 +0530

    Update LogInFormController.java

--- src/com/rc008code/hms/controller/LogInFormController.java
+++ src/com/rc008code/hms/controller/LogInFormController.java
@@ -1,8 +1,10 @@
 package com.rc008code.hms.controller;
 
 import com.rc008code.hms.auth.decorators.service.decorators.BasicAuthenticator;
+import com.rc008code.hms.auth.decorators.service.model.ValidationResult;
 import com.rc008code.hms.util.CommonUtil;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -16,7 +18,21 @@ public class LogInFormController {
 
     public void onLogInAction(ActionEvent actionEvent) throws IOException {
         BasicAuthenticator authenticator = new BasicAuthenticator();
-        authenticator.authenticate(txtUserName.getText(), txtPassword.getText());
-        new CommonUtil().setUi(context, "PharmasistDashboardForm");
+        ValidationResult authenticate = authenticator.authenticate(txtUserName.getText(), txtPassword.getText());
+        if (authenticate.isValid()) {
+            String message = authenticate.getMessage();
+            switch (message) {
+                case "admin":
+                    new CommonUtil().setUi(context, "AdminDashboardForm");
+                case "doctor":
+                    new CommonUtil().setUi(context, "DoctorDashboardForm");
+                case "nurse":
+                    new CommonUtil().setUi(context, "NurseDashboardForm");
+                case "pharmacist":
+                    new CommonUtil().setUi(context, "PharmasistDashboardForm");
+            }
+        } else {
+            new Alert(Alert.AlertType.ERROR, authenticate.getMessage()).show();
+        }
     }
 }
