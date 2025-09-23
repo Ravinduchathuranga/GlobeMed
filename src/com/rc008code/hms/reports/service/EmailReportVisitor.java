b36347847cbf3a4ad819368fdb9c12ba14997a63
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-23 22:48:32 +0530

    Create EmailReportVisitor.java

---
+++ src/com/rc008code/hms/reports/service/EmailReportVisitor.java
@@ -0,0 +1,28 @@
+package com.rc008code.hms.reports.service;
+
+import com.rc008code.hms.reports.api.ReportVisitor;
+import com.rc008code.hms.reports.element.PatientReportElement;
+import com.rc008code.hms.reports.util.MailManager;
+
+public class EmailReportVisitor implements ReportVisitor {
+    @Override
+    public void visit(PatientReportElement patientElement) {
+        try {
+            String subject = "Welcome Email";
+            String body = "Dear " + patientElement.getPatient().getName() + ",\n\n"
+                    + "Welcome to Global_med! We're excited to have you join us.\n\n"
+                    + "Here are your account details to get started:\n"
+                    + "User ID: " + patientElement.getPatient().getPatientId() + "\n"
+                    + "Email: " + patientElement.getPatient().getEmail() + "\n"
+                    + "Please use this information to sign in for the first time. For security purposes, we recommend changing your password after your initial login.\n\n"
+                    + "If you have any questions or need assistance, feel free to reach out to our support team.\n\n"
+                    + "Best regards,\n"
+                    + "The ViralCare Team";
+
+            MailManager.sendMail(patientElement.getPatient().getEmail(), subject, body);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
