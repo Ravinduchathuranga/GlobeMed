a2d039640c1e39ea2330275bad16fa071bc7b335
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-23 19:28:05 +0530

    Update UserNurseAuth.java

--- src/com/rc008code/hms/auth/decorators/service/handlers/UserNurseAuth.java
+++ src/com/rc008code/hms/auth/decorators/service/handlers/UserNurseAuth.java
@@ -1,6 +1,7 @@
 package com.rc008code.hms.auth.decorators.service.handlers;
 
 import com.rc008code.hms.auth.decorators.api.UserAuthHandler;
+import com.rc008code.hms.auth.decorators.service.model.ValidationResult;
 import com.rc008code.hms.business.BoFactory;
 import com.rc008code.hms.business.custom.AdminStaffBo;
 import com.rc008code.hms.business.custom.NurseBo;
@@ -9,14 +10,14 @@ public class UserNurseAuth extends UserAuthHandler {
     private static final NurseBo nurseBo = BoFactory.getInstance().getBo(BoFactory.BoType.NURSE);
 
     @Override
-    public boolean authenticate(String username, String password) {
+    public ValidationResult authenticate(String username, String password) {
         try {
             boolean b = nurseBo.logIn(username, password);
             if (b) {
-                return true;
+                return new ValidationResult(true, "nurse");
             } else if (nextHandler != null) {
                 return nextHandler.authenticate(username, password);
-            } else return false;
+            } else return new ValidationResult(false, "something went wrong");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
