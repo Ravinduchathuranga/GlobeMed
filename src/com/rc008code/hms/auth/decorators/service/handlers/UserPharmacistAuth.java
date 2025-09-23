f948cc577df5c32eb604f6ca76aaf7790a98dc53
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-23 19:28:06 +0530

    Update UserPharmacistAuth.java

--- src/com/rc008code/hms/auth/decorators/service/handlers/UserPharmacistAuth.java
+++ src/com/rc008code/hms/auth/decorators/service/handlers/UserPharmacistAuth.java
@@ -1,6 +1,7 @@
 package com.rc008code.hms.auth.decorators.service.handlers;
 
 import com.rc008code.hms.auth.decorators.api.UserAuthHandler;
+import com.rc008code.hms.auth.decorators.service.model.ValidationResult;
 import com.rc008code.hms.business.BoFactory;
 import com.rc008code.hms.business.custom.AdminStaffBo;
 import com.rc008code.hms.business.custom.PharmacistBo;
@@ -9,14 +10,14 @@ public class UserPharmacistAuth extends UserAuthHandler {
     private static final PharmacistBo pharmacistBo = BoFactory.getInstance().getBo(BoFactory.BoType.PHARMACIST);
 
     @Override
-    public boolean authenticate(String username, String password) {
+    public ValidationResult authenticate(String username, String password) {
         try {
             boolean b = pharmacistBo.logIn(username, password);
             if (b) {
-                return true;
+                return new ValidationResult(true, "pharmacist");
             } else if (nextHandler != null) {
                 return nextHandler.authenticate(username, password);
-            } else return false;
+            } else return new ValidationResult(false, "Invalid username or password");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
