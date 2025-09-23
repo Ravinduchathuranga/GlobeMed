91ee76cc905129891d3005408bcc262e4d56dd61
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-24 00:18:23 +0530

    Update NewPatientFormController.java

--- src/com/rc008code/hms/controller/NewPatientFormController.java
+++ src/com/rc008code/hms/controller/NewPatientFormController.java
@@ -1,26 +1,12 @@
 package com.rc008code.hms.controller;
 
-import com.rc008code.hms.business.BoFactory;
-import com.rc008code.hms.business.custom.PatientBo;
-import com.rc008code.hms.dto.PatientDto;
 import com.rc008code.hms.enums.Gender;
-import com.rc008code.hms.util.AlertBuilder;
-import com.rc008code.hms.util.CommonUtil;
 import com.rc008code.hms.view.tableModels.PatientTM;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-public class PatientFormController {
+public class NewPatientFormController {
 
     // Root context
     public AnchorPane context;
@@ -52,6 +38,7 @@ public class PatientFormController {
    public void initialize() {
 
    }
+
 
     public void OnBackToHome(ActionEvent event) {
     }
