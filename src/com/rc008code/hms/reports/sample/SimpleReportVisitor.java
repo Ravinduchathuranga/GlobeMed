022683dee0e5ae26fb487a3ab3673674b2559575
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-24 06:46:22 +0530

    Update SimpleReportVisitor.java

--- src/com/rc008code/hms/reports/sample/SimpleReportVisitor.java
+++ src/com/rc008code/hms/reports/sample/SimpleReportVisitor.java
@@ -2,11 +2,8 @@ package com.rc008code.hms.reports.sample;
 
 import com.rc008code.hms.reports.api.ReportVisitor;
 import com.rc008code.hms.reports.element.PatientReportElement;
+import com.rc008code.hms.reports.element.AppointmentReportElement;
 
-/**
- * A simple text report visitor that aggregates details of visited elements
- * into a human-readable String.
- */
 public class SimpleReportVisitor implements ReportVisitor {
     private final StringBuilder builder = new StringBuilder();
 
@@ -24,6 +21,16 @@ public class SimpleReportVisitor implements ReportVisitor {
                .append("Contact   : ").append(nullSafe(patientElement.getPatient().getContact())).append(System.lineSeparator())
                .append("Email     : ").append(nullSafe(patientElement.getPatient().getEmail())).append(System.lineSeparator())
                .append("Address   : ").append(nullSafe(patientElement.getPatient().getAddress())).append(System.lineSeparator())
+               .append("----------------------------------------").append(System.lineSeparator());
+    }
+
+    @Override
+    public void visit(AppointmentReportElement appointmentElement) {
+        builder.append("Appointment ID: ").append(nullSafe(appointmentElement.getAppointment().getAppointmentId())).append(System.lineSeparator())
+               .append("Patient       : ").append(nullSafe(appointmentElement.getPatient().getName())).append(System.lineSeparator())
+               .append("Doctor        : ").append(nullSafe(appointmentElement.getDoctor().getName())).append(System.lineSeparator())
+               .append("When          : ").append(String.valueOf(appointmentElement.getAppointment().getAppointmentDate())).append(System.lineSeparator())
+               .append("Description   : ").append(nullSafe(appointmentElement.getAppointment().getDescription())).append(System.lineSeparator())
                .append("----------------------------------------").append(System.lineSeparator());
     }
 
