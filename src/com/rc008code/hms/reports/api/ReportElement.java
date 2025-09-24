bcaa930ccefecb4ed8417d9588c94f36d8db6536
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2025-09-24 06:46:17 +0530

    Update ReportElement.java

--- src/com/rc008code/hms/reports/api/ReportElement.java
+++ src/com/rc008code/hms/reports/api/ReportElement.java
@@ -1,8 +1,6 @@
 package com.rc008code.hms.reports.api;
 
-/**
- * Marker interface for reportable elements that can be visited by a ReportVisitor.
- */
+
 public interface ReportElement {
     void accept(ReportVisitor visitor);
 }
